9bf53cf66d16f091dfb4be4074e431d549fa190f
Doke <81121062+Doke1@users.noreply.github.com>
2022-10-01 14:42:02 -0500

    Update Curl.php

--- Curl.php
+++ Curl.php
@@ -5,23 +5,23 @@
     {
         public static function send(string $url)
         {
-            $MAXCONN = 1;
-            $TIMEOUT = 10;
+            $MAX_CONNECTS = 1;
+            $TIMEOUT = 2;
 
             $headers = array(
                 "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+                "Keep-Alive: true",
             );
 
             $curl = curl_init();
 
             curl_setopt($curl, CURLOPT_URL, $url);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_MAXCONNECTS, $MAXCONN);
+            curl_setopt($curl, CURLOPT_MAXCONNECTS, $MAX_CONNECTS);
             curl_setopt($curl, CURLOPT_TIMEOUT, $TIMEOUT);
+            curl_setopt($curl, CURLOPT_ENCODING, '');
             curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-            
-            
-           	
+
             $response = curl_exec($curl);
             curl_close($curl);
 
