1050d66654db78b4fdd211631094d3bfc875bcd1
Doke <81121062+Doke1@users.noreply.github.com>
2022-10-02 16:21:37 -0500

    Update init.php

--- init.php
+++ init.php
@@ -4,6 +4,7 @@
 
     $a = new GetPlayers();
     $country = readline("Country: ");
+    print_r("\033[1mList of Roles: Rifler, AWPers, coach\033[0m\r\n");
     $role = readline("Role: ");
     $age = readline("Age: ");
     $majors = readline("Majors: ");
@@ -14,4 +15,6 @@
     if(preg_match($str, $country) && preg_match($str, $role) && preg_match("/\d+/", $age) && preg_match("/\d+/", $majors)) {
         $a->get($country, $role, $age, $majors);
     } else echo "\r\nInvalid filters\r\n";
+
+    sleep(30);
 ?>
